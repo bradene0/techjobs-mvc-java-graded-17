4571a83e28f1d3a6f327cc1db8bb29f67b751a6a
Braden E <bradene0@outlook.com>
2023-10-10 18:42:57 -0500

    All tasks complete, failing 4 tests

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,27 +1,41 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping; // Use @PostMapping
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
+import java.util.ArrayList;
 
-
-/**
- * Created by LaunchCode
- */
 @Controller
 @RequestMapping("search")
 public class SearchController {
 
     @GetMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", columnChoices);
+        model.addAttribute("columns", ListController.columnChoices); // Use ListController.columnChoices
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    // Add a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model,
+                                       String searchType, // Use the correct parameter names
+                                       String searchTerm) {
+        ArrayList<Job> jobs;
 
+        if (searchType.equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+// Use ListController.columnChoices
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
-
